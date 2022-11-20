af99acbab50eb5eb424f9936fe0da4b9b9fc480a
desperadoccy <527707607@qq.com>
2022-11-20 20:15:08 +0800

    根据inuaa限制更新登陆校验

--- inuaa/sign.py
+++ inuaa/sign.py
@@ -7,18 +7,16 @@ import re
 import json
 from send_mail import send_mail
 
-
 # 如果发包过快会造成502，如果给多个同学打卡请注意一下请求速度
 try_times = 2
 
 # 每次requests请求的延迟(s秒)，太低会封IP
 delay = 3
 
-
 '''
 教务处的垃圾命名规则
 info: {
-                sfzhux: 0,
+                sfzhux: 1,
                 zhuxdz: '',
                 szgj: '',
                 szcs: '',
@@ -82,12 +80,12 @@ info: {
                 // dsdecjcsj:"", //倒数第二次核酸检测时间
                 // dsdechsjcjgtype:"", //倒数第二次核酸检测地点
                 // dsdrchsjcdd:"",  //倒数第二次核酸检测地点
-                
+
                 // dsdechsjcjg:"",  //倒数第二次核酸检测结果
                 // zhyccjcsj:"", //最后一次核酸检测时间
                 // zhycchsjcjgtype:"", //最后一次核酸检测地点
                 // zhycchsjcdd:"",  //最后一次核酸检测地点
-                
+
                 // zhycchsjcjg:"",  //最后一次核酸检测结果
                 wskmyy:"",//请简要说明无苏康码原因
                 zhycjgdqifjn:"",//7月10日后最后一次经过中高风险区的地区为中国境内还是中国境外
@@ -104,6 +102,7 @@ info: {
 }
 '''
 
+
 def get_uid_id(cookies):
     '''
     获取id与uid
@@ -112,7 +111,7 @@ def get_uid_id(cookies):
         try:
             time.sleep(delay)
             response = requests.get(
-                'https://m.nuaa.edu.cn/ncov/wap/default', cookies=cookies)
+                'https://m.nuaa.edu.cn/ncov/wap/default/index?t=20210430', cookies=cookies)
             response.encoding = 'utf-8'
 
             # print(response.text)
@@ -120,36 +119,29 @@ def get_uid_id(cookies):
             uid = re.search(r'"uid":"([0-9]*)"', response.text).group(1)
             id = re.search(r'"id":([0-9]*)', response.text).group(1)
             print(uid, id)
-            return uid,id
+            return uid, id
         except Exception as e:
             print(e)
     # 就这样吧，让他崩溃，万一假打卡了就不好了
     print('获取id、uid失败')
     return False, '获取id、uid失败\n'
 
-def login(login_id, login_password):
+
+def login(login_id, login_password, imei, mobile_type):
     '''
     登陆I南航，返回Cookie，失败返回空串
     '''
     # headers['Cookie'] = ''
-    cookies = ''
+    cookies = dict()
     for _ in range(try_times):
         try:
             time.sleep(delay)
-            r = requests.get(
-                'https://m.nuaa.edu.cn/uc/wap/login', cookies=cookies)
-            print('get login page:', r.status_code)
-           
-            cookies = dict(r.cookies)
-            # print(r.cookies)
-
-
-            time.sleep(delay)
-            r = requests.get('https://m.nuaa.edu.cn/uc/wap/login/check', cookies=cookies,
-                             data='username={}&password={}'.format(login_id, login_password))
+            r = requests.get('https://m.nuaa.edu.cn/a_nuaa/api/login-v2/index', cookies=cookies,
+                             data='username={}&password={}&imei={}&mobile_type={}&sid={}'.format(login_id,
+                                                                                                 login_password, imei,
+                                                                                                 mobile_type, imei))
             print('login...:', r.status_code)
 
-        
             cookies.update(dict(r.cookies))
 
             # headers['Cookie'] = cookie
@@ -164,7 +156,6 @@ def login(login_id, login_password):
 
 
 def sign(m, d, user, smtp_host, mail_username, mail_password):
-
     '''
     签到，并且发送邮件提醒，成功返回True，失败返回False
     '''
@@ -192,11 +183,25 @@ def sign(m, d, user, smtp_host, mail_username, mail_password):
                 'jcjgqr': '0',
                 'remark': '',
                 'address': '江苏省南京市江宁区秣陵街道慧园路南京航空航天大学将军路校区',
-                'geo_api_info': {"type":"complete","position":{"Q":31.939607,"R":118.791155,"lng":118.791155,"lat":31.939607},"location_type":"html5","message":"Get geolocation success.Don't need convert.Get address success.","accuracy":29,"isConverted":1,"status":1,"addressComponent":{"citycode":"025","adcode":"320115","businessAreas":[{"name":"开发区","id":"320115","location":{"Q":31.925973,"R":118.80980399999999,"lng":118.809804,"lat":31.925973}}],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"将军大道","streetNumber":"29号","country":"中国","province":"江苏省","city":"南京市","district":"江宁区","towncode":"320115011000","township":"秣陵街道"},"formattedAddress":"江苏省南京市江宁区秣陵街道慧园路南京航空航天大学将军路校区","roads":[],"crosses":[],"pois":[],"info":"SUCCESS"},
+                'geo_api_info': {"type": "complete",
+                                 "position": {"Q": 31.939607, "R": 118.791155, "lng": 118.791155, "lat": 31.939607},
+                                 "location_type": "html5",
+                                 "message": "Get geolocation success.Don't need convert.Get address success.",
+                                 "accuracy": 29, "isConverted": 1, "status": 1,
+                                 "addressComponent": {"citycode": "025", "adcode": "320115", "businessAreas": [
+                                     {"name": "开发区", "id": "320115",
+                                      "location": {"Q": 31.925973, "R": 118.80980399999999, "lng": 118.809804,
+                                                   "lat": 31.925973}}], "neighborhoodType": "", "neighborhood": "",
+                                                      "building": "", "buildingType": "", "street": "将军大道",
+                                                      "streetNumber": "29号", "country": "中国", "province": "江苏省",
+                                                      "city": "南京市", "district": "江宁区",
+                                                      "towncode": "320115011000", "township": "秣陵街道"},
+                                 "formattedAddress": "江苏省南京市江宁区秣陵街道慧园路南京航空航天大学将军路校区",
+                                 "roads": [], "crosses": [], "pois": [], "info": "SUCCESS"},
                 'area': '江苏省 南京市 江宁区',
                 'province': '江苏省',
                 'city': '南京市',
-                'sfzx': '0',
+                'sfzx': 1,
                 'sfjcwhry': '0',
                 'sfjchbry': '0',
                 'sfcyglq': '0',
@@ -212,7 +217,7 @@ def sign(m, d, user, smtp_host, mail_username, mail_password):
                 'jcjg': '',
                 'date': time.strftime("%Y%m%d", time.localtime()),  # 打卡年月日一共8位
                 'uid': user['uid'],  # UID
-                'created': round(time.time()), # 时间戳
+                'created': round(time.time()),  # 时间戳
                 'jcqzrq': '',
                 'sfjcqz': '',
                 'szsqsfybl': '0',
@@ -221,16 +226,16 @@ def sign(m, d, user, smtp_host, mail_username, mail_password):
                 'sfygtjzzfj': '0',
                 'gtjzzfjsj': '',
                 'created_uid': '0',
-                'id': user['id'],# 打卡的ID，其实这个没影响的
+                'id': user['id'],  # 打卡的ID，其实这个没影响的
                 'gwszdd': '',
                 'sfyqjzgc': '',
                 'jrsfqzys': '',
                 'jrsfqzfy': '',
                 'ismoved': '0',
-                'dqsfzgfxszqs':'0', #当前是否在低风险地区
-                'bfhzjyq': '3', #不符合接种要求
-                'cjfxsfhs': '1', #春季返校后是否坐核酸
-                'gzczxq': '2', #工作常驻校区
+                'dqsfzgfxszqs': '0',  # 当前是否在低风险地区
+                'bfhzjyq': '3',  # 不符合接种要求
+                'cjfxsfhs': '1',  # 春季返校后是否坐核酸
+                'gzczxq': '2',  # 工作常驻校区
                 'sfjkyc': '0',
                 'sfmrhs': '1',
                 'ifcyglq': '0',
@@ -242,12 +247,12 @@ def sign(m, d, user, smtp_host, mail_username, mail_password):
             print('sign statue code:', r.status_code)
             print('sign return:', r.text)
             r.encoding = 'utf-8'
-            
+
             if r.text.find('成功') >= 0:
                 print('打卡成功')
                 if user['receiver_mail'] != '':
                     send_mail(mail_username, mail_password, smtp_host,
-                              user['receiver_mail'], user['name']+'校外打卡成功', '校外打卡成功', user['name'], '老王')
+                              user['receiver_mail'], user['name'] + '打卡成功', '打卡成功', user['name'], '打卡机器人')
                 return True
             else:
                 print('打卡失败，尝试重新登陆')
@@ -259,44 +264,42 @@ def sign(m, d, user, smtp_host, mail_username, mail_password):
             # print(r.request.body)
     if user['receiver_mail'] != '':
         send_mail(mail_username, mail_password, smtp_host,
-                  user['receiver_mail'], user['name']+'校外打卡GG', '校外打卡GG', user['name'], '老王')
+                  user['receiver_mail'], user['name'] + '校外打卡GG', '校外打卡GG', user['name'], '老王')
     return False
 
 
 def main():
     print('------>>>---->启动中<------<<<----')
-    last_post = 10086   # 最后一次签到的日期
-    
+    last_post = 10086  # 最后一次签到的日期
+
     # 读取配置文件
-    with open('config.json', 'r',encoding='UTF-8') as f:
+    with open('config.json', 'r', encoding='UTF-8') as f:
         config = json.loads(f.read())
     smtp_host = config['smtp_host']
     mail_password = config['mail_password']
     mail_username = config['mail_username']
     users = config['users']
-    
-    # 一起登陆啊，失败了就先空着，等打卡时候再来管他
-    
-   
 
-    
+    # 一起登陆啊，失败了就先空着，等打卡时候再来管他
+
     # 每一天先拷贝一下需要打卡的列表，然后打卡
     # 其他时间如果监测到待打卡的列表非空，就重新读打卡
-    
+
     while True:
         t = time.localtime()
 
         if t.tm_mday != last_post:
             for user in users:
                 print('Login...:', user['name'])
-                user['cookie'], user['uid'], user['id'] = login(user['student_id'], user['student_password'])
+                user['cookie'], user['uid'], user['id'] = login(user['student_id'], user['student_password'],
+                                                                user['imei'], user['mobile_type'])
             # 新的一天，拷贝一份完整的打卡清单，全部打一遍卡。但是这样做的话每次更新cookie，users也自动更新。
 
             print('----------开始每日打卡----------')
             to_sign_list = users.copy()
 
             # 给每个人打卡
-            new_list = []   # 未完成打卡的暂时放这里
+            new_list = []  # 未完成打卡的暂时放这里
             for user in to_sign_list[:]:
                 print('**********' + user['name'] + '**********')
                 if sign(t.tm_mon, t.tm_mday, user, smtp_host, mail_username, mail_password):
@@ -307,18 +310,18 @@ def main():
                         user['name'], t.tm_mon, t.tm_mday))
                     new_list.append(user)
             to_sign_list = new_list
-            last_post = t.tm_mday   # 更新日期
+            last_post = t.tm_mday  # 更新日期
 
         elif len(to_sign_list) != 0:
             # 一天中的后续尝试，先等待2小时，然后再打卡
-            time.sleep(3600)    # 失败用户每一个小时重试一次
-        
+            time.sleep(3600)  # 失败用户每一个小时重试一次
+
             print('----------重新打卡尝试----------')
             # 给每个失败的人打卡
             for user in users:
-                print('Login...:', user['name']) 
+                print('Login...:', user['name'])
                 user['cookie'], user['uid'], user['id'] = login(user['student_id'], user['student_password'])
-            new_list = []   # 未完成打卡的暂时放这里
+            new_list = []  # 未完成打卡的暂时放这里
             for user in to_sign_list[:]:
                 print('**********' + user['name'] + '**********')
                 if sign(t.tm_mon, t.tm_mday, user, smtp_host, mail_username, mail_password):
@@ -331,6 +334,7 @@ def main():
             to_sign_list = new_list
         # else: 都打完了
         time.sleep(2)
+
 
 if __name__ == '__main__':
     main()
